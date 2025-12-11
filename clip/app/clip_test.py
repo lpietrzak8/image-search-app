a2809a3501573bb7902cf92f6bcdd0c19bad20b2
Łukasz Pietrzak <lukasz30105@gmail.com>
2025-12-11 13:58:19 +0100

    added script to train model on RSICD images

--- clip/app/clip_test.py
+++ clip/app/clip_test.py
@@ -5,25 +5,70 @@ from transformers import CLIPProcessor, CLIPModel
 import torch
 import math
 from PIL import Image
+import matplotlib.pyplot as plt
 
 
 import time
 import pickle
 from model import ClipModel
 import ranking
+from datasets import load_dataset
+
+
+dataset = load_dataset("arampacha/rsicd")
+train_subset = dataset['train']
+eval_subset = dataset['valid']
 start_time = time.time()
-
-clipModel = ClipModel()
-
-
-if not os.path.isfile("clip/embeddings.pkl"):
-   clipModel.compute_image_embeddings("resources")
+# "/Volumes/SANDISK/clip-arampacha-finetuned"
+clipModel = ClipModel("app/clip-lora-output")
 
 
-image_embeddings, image_files = clipModel.load_embeddings("clip/embeddings.pkl")
+print(f"clip model loaded in")
+if not os.path.isfile("embeddings_subset2.pkl"):
+    embeddings = []
+    files = []
 
-results = ranking.rank_images(clipModel, "city", image_embeddings, image_files)
+    for i in range(len(eval_subset)):
+        print(f"im doing {i}")
+        item = eval_subset[i]
+        pil_image = item['image'].convert("RGB")
+        emb = clipModel.get_image_embedding(pil_image)
+        embeddings.append(emb.cpu())
+        files.append(f"rsicd_{i}.jpg")
+
+    embeddings = torch.vstack(embeddings)
+    with open("embeddings_subset2.pkl", "wb") as f:
+        pickle.dump((embeddings, files), f)
+
+
+
+image_embeddings, image_files = clipModel.load_embeddings("embeddings_subset2.pkl")
+
+query="tall buildings"
+results = ranking.rank_images(clipModel, query, image_embeddings, image_files)
 
 ranking.print_ranking(results, 10)
+
+
+
+dataset = load_dataset("arampacha/rsicd")
+train_subset = dataset['train']
+
+
+top_results = results[:10]
+
+plt.figure(figsize=(15, 5), num=query)
+for i, (fname, score) in enumerate(top_results):
+   
+    idx = int(fname.split('_')[1].split('.')[0])
+    img = eval_subset[idx]['image'].convert("RGB")
+
+    ax = plt.subplot(2, 5, i + 1)
+    plt.imshow(img)
+    plt.title(f"{fname}\n{score:.2f}")
+    plt.axis("off")
+
+plt.tight_layout()
+plt.show()
     
 # print("--- %s seconds ---" % (time.time() - start_time))
